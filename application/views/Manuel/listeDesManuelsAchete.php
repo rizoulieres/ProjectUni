a7646abb8bcdb9a6cc1958a5ad7d6dd4e66587ed
Perez Alexis <perez_alexis@hotmail.fr>
2019-06-24 20:44:17 +0200

    ajout des manuels réservé

--- application/views/Manuel/listeDesManuelsAchete.php
+++ application/views/Manuel/listeDesManuelsAchete.php
@@ -1,7 +1,7 @@
 
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Liste de mes manuels Vendus</h1>
+    <h1 class="h3 mb-0 text-gray-800">Liste de mes manuels Achetés</h1>
 </div>
 
 <div class="row">
